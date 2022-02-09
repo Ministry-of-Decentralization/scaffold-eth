6870b2c7bc11f734790ba7ff07503fa961b6fffc
Steven Ciraolo <steven.ciraolo@gmail.com>
2022-02-09 13:44:41 -0800

    updated subgraph

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -2,7 +2,7 @@ import { BigInt, Address } from "@graphprotocol/graph-ts";
 import {
   BrickUpdated,
   PixelBoardInit,
-} from "../generated/YourContract/YourContract";
+} from "../generated/PixelBoard/PixelBoard";
 import { Brick, Wall } from "../generated/schema";
 
 let wallPositions = [
@@ -14,29 +14,53 @@ let wallPositions = [
   'ceiling'
 ]
 
-export function handleBrickUpdated(event: BrickUpdated): void {
-  let senderString = event.params.sender.toHexString();
+let brickRowCount = 150;
 
-  let sender = Sender.load(senderString);
-
-  if (sender === null) {
-    sender = new Sender(senderString);
-    sender.address = event.params.sender;
-    sender.createdAt = event.block.timestamp;
-    sender.purposeCount = BigInt.fromI32(1);
-  } else {
-    sender.purposeCount = sender.purposeCount.plus(BigInt.fromI32(1));
+function getOrCreateWall(id: string): Wall {
+  let wall = Wall.load(id)
+  if (wall == null) {
+    wall = new Wall(id);
+    wall.rowCount = brickRowCount;
+    wall.save()
   }
 
-  let purpose = new Purpose(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  );
+  return wall
+}
 
-  purpose.purpose = event.params.purpose;
-  purpose.sender = senderString;
-  purpose.createdAt = event.block.timestamp;
-  purpose.transactionHash = event.transaction.hash.toHex();
+function getOrCreateBrick(_wall: i32, _x:i32, _y: i32 ): Brick {
+  let wallId = wallPositions[_wall]
+  let wall = getOrCreateWall(wallId)
+  let id = wall.id.toString() + '_' + BigInt.fromI32(_x).toString() + '_' + BigInt.fromI32(_y).toString()
+  let brick = Brick.load(id)
 
-  purpose.save();
-  sender.save();
+  if (brick == null) {
+    brick = new Brick(id)
+    brick.wall = wallId
+    brick.x = _x
+    brick.y = _y
+    brick.rgb = '0x000000'
+    brick.owner = Address.fromString('0x0')
+    brick.price = BigInt.fromI32(0)
+    brick.lastUpdated = BigInt.fromI32(0)
+    brick.save()
+  }
+
+  return brick
+}
+
+export function handleBrickUpdated(event: BrickUpdated): void {
+  let brick = getOrCreateBrick(event.params.wall, event.params.x, event.params.y);
+
+  brick.owner = event.transaction.from
+  brick.rgb = event.params.rgb.toString()
+  brick.price = event.params.price
+  brick.lastUpdated = event.block.timestamp
+
+  brick.save()
+}
+
+export function handlePixelBoardInit(event: PixelBoardInit): void {
+  for (let i = 0; i < wallPositions.length;i++) {
+    getOrCreateWall(wallPositions[i])
+  }
 }
